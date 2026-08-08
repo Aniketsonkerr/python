32550ed2caf95a2576fe1d5cb4df373746fdba7f
aniketsonkerr <aniketsonkerr@gmail.com>
2026-08-08 17:08:22 +0530

    strings and their methos

---
+++ strings.py
@@ -0,0 +1,24 @@
+#string and thier methods 
+#string is immutable whenever perform any methods on string it returns new a string
+name="tony stark"
+
+#methods 
+
+# #1.upper:
+# print(name.upper(),":it's new string not the original one.")
+# print(name,":here is the orignal one")
+
+# #2.find: it returns the index of the character in the string 
+# # string follows 0 based index 
+# position = (name.find("k"))
+# print(position)
+
+#3.replace
+# print(name.replace("tony stark","iron man"))
+# print(name)
+
+# #4.check://returns bool value 
+# print("s" in name)
+
+# #reserved keyword
+# in , for , while , True   
