6b462bd3bad0c76c26847d105e560ddcd99eb093
aniketsonkerr <aniketsonkerr@gmail.com>
2026-08-09 09:40:09 +0530

    list intro

---
+++ list.py
@@ -0,0 +1,12 @@
+lst=[100,101,400,500]
+# print(lst[0])
+
+# for x in lst:
+#     print(x)
+
+#valid indices for a list is from 0 - len(lst)-1
+
+for x in range(0,len(lst)):
+    print(lst[x])
+
+print("length:",len(lst)-1)
