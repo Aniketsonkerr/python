edded8645b109e7eef7773d805d633b8c17a34e8
aniketsonkerr <aniketsonkerr@gmail.com>
2026-08-17 17:15:36 +0530

    pandas data processing null removing

---
+++ .ipynb_checkpoints/functions-checkpoint.py
@@ -0,0 +1,14 @@
+
+# def greet(n):
+#     print("hello",n)
+# name = input("enter your name :")
+
+# greet(name)
+
+def nEvenNumber(n):
+    for i in range(2,n,2):
+        print(i)
+
+evenNumber = int(input("want even number till: "))
+
+nEvenNumber(evenNumber)
